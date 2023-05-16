c5359a3c36179bed357f15ea02d07df53b18af8c
siberian <siberian@sibprojects.com>
2023-05-16 21:20:26 +0100

    add

---
+++ Prototype/ComponentWithBackReference.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Prototype;
+
+class ComponentWithBackReference
+{
+    public $prototype;
+
+    /**
+     * Note that the constructor will not be executed during
+     * cloning. If you have complex logic inside a constructor, you may
+     * need to be done in the clone method as well.
+     */
+    public function __construct(Prototype $prototype)
+    {
+        $this->prototype = $prototype;
+
+    }
+}

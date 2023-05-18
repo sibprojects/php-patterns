db80b0b95a8bf2696cd38092cd35dc1cdd8911ba
siberian <siberian@sibprojects.com>
2023-05-18 17:40:30 +0100

    Add

---
+++ Singleton/index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Singleton;
+
+$s1 = Singleton::getInstance();
+$s2 = Singleton::getInstance();
+if ($s1 === $s2) {
+    echo "Singleton works, both variables contain the same instance.";
+} else {
+    echo "Singleton failed, variables contain different instances.";
+}

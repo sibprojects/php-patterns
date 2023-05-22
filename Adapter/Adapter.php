9ba16181f0f39f92bb3dd9ee320f1364b60c314e
siberian <siberian@sibprojects.com>
2023-05-22 15:28:24 +0100

    Add

---
+++ Adapter/Adapter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Adapter;
+
+/**
+ * The adapter makes the interface of the Adapted class compatible with the target
+ * interface.
+ */
+class Adapter extends Target
+{
+    private $adaptee;
+
+    public function __construct(Adaptee $adaptee)
+    {
+        $this->adaptee = $adaptee;
+    }
+
+    public function request(): string
+    {
+        return "Adapter: (TRANSLATED) " . strrev($this->adaptee->specificRequest());
+    }
+}

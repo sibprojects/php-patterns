257ad8b844e317d688093b7ed47bcd297b7b848d
siberian <siberian@sibprojects.com>
2023-05-23 18:27:30 +0100

    Add

---
+++ Bridge/BridgeAbstraction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Bridge;
+
+/**
+ * Abstraction establishes an interface for the "control" part of the two hierarchies
+ * classes. It contains a reference to an object from the Implementation hierarchy and delegates
+ * him all the real work.
+ */
+class BridgeAbstraction
+{
+    /**
+     * @var Implementation
+     */
+    protected $implementation;
+
+    public function __construct(Implementation $implementation)
+    {
+        $this->implementation = $implementation;
+    }
+
+    public function operation(): string
+    {
+        return "Abstraction: Base operation with:\n" .
+            $this->implementation->operationImplementation();
+    }
+}

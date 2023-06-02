50ba6b85ca41be918ab721d5f3894d62cb141ec5
siberian <siberian@sibprojects.com>
2023-06-02 03:06:10 +0100

    Add

---
+++ Facade/Subsystem2.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace RefactoringGuru\Facade\Conceptual;
+
+/**
+ * Some facades can work with different subsystems at the same time.
+ */
+class Subsystem2
+{
+    public function operation1(): string
+    {
+        return "Subsystem2: Get ready!\n";
+    }
+
+    // ...
+
+    public function operationZ(): string
+    {
+        return "Subsystem2: Fire!\n";
+    }
+}

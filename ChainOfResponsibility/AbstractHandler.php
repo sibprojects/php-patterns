dd34f1ea951d4f34b9107c41745c2dd2da06febe
siberian <siberian@sibprojects.com>
2023-06-09 09:04:28 +0100

    Add

---
+++ ChainOfResponsibility/AbstractHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ChainOfResponsibility;
+
+/**
+ * Default chaining behavior can be implemented inside the base class
+ * handler.
+ */
+abstract class AbstractHandler implements Handler
+{
+    private $nextHandler;
+
+    public function setNext(Handler $handler): Handler
+    {
+        $this->nextHandler = $handler;
+        // Returning a handler from here will allow us to bind handlers with a simple
+        // way, like this:
+        // $monkey->setNext($squirrel)->setNext($dog);
+        return $handler;
+    }
+
+    public function handle(string $request): ?string
+    {
+        if ($this->nextHandler) {
+            return $this->nextHandler->handle($request);
+        }
+
+        return null;
+    }
+}

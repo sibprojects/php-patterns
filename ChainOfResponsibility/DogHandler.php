73821afa7da245f755d675878cec2f96d019ea73
siberian <siberian@sibprojects.com>
2023-06-12 01:38:32 +0100

    Add

---
+++ ChainOfResponsibility/DogHandler.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ChainOfResponsibility;
+
+class DogHandler extends AbstractHandler
+{
+    public function handle(string $request): ?string
+    {
+        if ($request === "MeatBall") {
+            return "Dog: I'll eat the " . $request . ".\n";
+        } else {
+            return parent::handle($request);
+        }
+    }
+}

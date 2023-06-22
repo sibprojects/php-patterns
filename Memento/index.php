d996351dcb587150069b21729911f68975da96b0
siberian <siberian@sibprojects.com>
2023-06-22 02:09:36 +0100

    Add

---
+++ Memento/index.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Memento;
+
+/**
+ * Client code
+ */
+$originator = new Originator("Super-duper-super-puper-super.");
+$caretaker = new Caretaker($originator);
+
+$caretaker->backup();
+$originator->doSomething();
+
+$caretaker->backup();
+$originator->doSomething();
+
+$caretaker->backup();
+$originator->doSomething();
+
+echo "\n";
+$caretaker->showHistory();
+
+echo "\nClient: Now, let's rollback!\n\n";
+$caretaker->undo();
+
+echo "\nClient: Once more!\n\n";
+$caretaker->undo();

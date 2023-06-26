da5085b6ffeae6955129300d97785d6f3e8c3044
siberian <siberian@sibprojects.com>
2023-06-26 23:00:06 +0100

    Add

---
+++ State/ConcreteStateA.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace State;
+
+/**
+ * Concrete States implement different behaviors associated with
+ * the state of the Context.
+ */
+class ConcreteStateA extends State
+{
+    public function handle1(): void
+    {
+        echo "ConcreteStateA handles request1.\n";
+        echo "ConcreteStateA wants to change the state of the context.\n";
+        $this->context->transitionTo(new ConcreteStateB());
+    }
+
+    public function handle2(): void
+    {
+        echo "ConcreteStateA handles request2.\n";
+    }
+}

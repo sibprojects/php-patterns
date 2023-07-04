7708138d3d23829e8197d8b486fec73aaa610b67
siberian <siberian@sibprojects.com>
2023-07-04 17:17:21 +0100

    Add

---
+++ Command/SimpleCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Command;
+
+/**
+ * Some teams are able to perform simple operations on their own.
+ */
+class SimpleCommand implements Command
+{
+    private $payload;
+
+    public function __construct(string $payload)
+    {
+        $this->payload = $payload;
+    }
+
+    public function execute(): void
+    {
+        echo "SimpleCommand: See, I can do simple things like printing (" . $this->payload . ")\n";
+    }
+}

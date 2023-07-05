11b416c70997b393f48b1e137b81ab718eef27db
siberian <siberian@sibprojects.com>
2023-07-05 19:26:57 +0100

    Add

---
+++ Command/index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Command;
+
+/**
+ * The client code can parameterize the sender with any commands.
+ */
+$invoker = new Invoker();
+$invoker->setOnStart(new SimpleCommand("Say Hi!"));
+$receiver = new Receiver();
+$invoker->setOnFinish(new ComplexCommand($receiver, "Send email", "Save report"));
+
+$invoker->doSomethingImportant();

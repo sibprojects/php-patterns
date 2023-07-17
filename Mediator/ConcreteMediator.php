a004b46eb53297c7ec9e0b662b25da99fcf0e022
siberian <siberian@sibprojects.com>
2023-07-17 17:19:10 +0100

    Add

---
+++ Mediator/ConcreteMediator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Mediator;
+
+/**
+ * Concrete Intermediaries implement collaborative behavior by coordinating individual
+ * Components.
+ */
+class ConcreteMediator implements Mediator
+{
+    private $component1;
+
+    private $component2;
+
+    public function __construct(Component1 $c1, Component2 $c2)
+    {
+        $this->component1 = $c1;
+        $this->component1->setMediator($this);
+        $this->component2 = $c2;
+        $this->component2->setMediator($this);
+    }
+
+    public function notify(object $sender, string $event): void
+    {
+        if ($event == "A") {
+            echo "Mediator reacts on A and triggers following operations:\n";
+            $this->component2->doC();
+        }
+
+        if ($event == "D") {
+            echo "Mediator reacts on D and triggers following operations:\n";
+            $this->component1->doB();
+            $this->component2->doC();
+        }
+    }
+}

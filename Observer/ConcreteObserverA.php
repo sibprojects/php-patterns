ca165a2790739a1c316b56f28698f144cc83818c
siberian <siberian@sibprojects.com>
2023-07-24 17:58:58 +0100

    Add observer

---
+++ Observer/ConcreteObserverA.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Observer;
+
+/**
+ * Specific Observers respond to updates released by the Publisher to
+ * to which they are attached.
+ */
+class ConcreteObserverA implements \SplObserver
+{
+    public function update(\SplSubject $subject): void
+    {
+        if ($subject->state < 3) {
+            echo "ConcreteObserverA: Reacted to the event.\n";
+        }
+    }
+}

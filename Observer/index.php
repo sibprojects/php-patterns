936bfc5d92ab35bbb46681a4aec8e57333c78ebb
siberian <siberian@sibprojects.com>
2023-07-24 17:59:14 +0100

    Add observer

---
+++ Observer/index.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Observer;
+
+$subject = new Subject();
+
+$o1 = new ConcreteObserverA();
+$subject->attach($o1);
+
+$o2 = new ConcreteObserverB();
+$subject->attach($o2);
+
+$subject->someBusinessLogic();
+$subject->someBusinessLogic();
+
+$subject->detach($o2);
+
+$subject->someBusinessLogic();

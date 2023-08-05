a86328ca4dcd857e963ec9cb6d3fd8c4530eb640
siberian <siberian@sibprojects.com>
2023-08-05 14:23:19 +0100

    Add Visitor

---
+++ Visitor/ConcreteVisitor2.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Visitor;
+
+class ConcreteVisitor2 implements Visitor
+{
+    public function visitConcreteComponentA(ConcreteComponentA $element): void
+    {
+        echo $element->exclusiveMethodOfConcreteComponentA() . " + ConcreteVisitor2\n";
+    }
+
+    public function visitConcreteComponentB(ConcreteComponentB $element): void
+    {
+        echo $element->specialMethodOfConcreteComponentB() . " + ConcreteVisitor2\n";
+    }
+}

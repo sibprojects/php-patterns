e893c16bb42607d7fcc21f2a5f734249099c2a57
siberian <siberian@sibprojects.com>
2023-08-08 00:20:10 +0100

    Add index

---
+++ Visitor/index.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Visitor;
+
+/**
+ * Client code can perform visitor operations on any set
+ * elements without figuring out their specific classes. The accept operation directs
+ * call to the corresponding operation in the visitor object.
+ */
+function clientCode(array $components, Visitor $visitor)
+{
+    // ...
+    foreach ($components as $component) {
+        $component->accept($visitor);
+    }
+    // ...
+}
+
+$components = [
+    new ConcreteComponentA(),
+    new ConcreteComponentB(),
+];
+
+echo "The client code works with all visitors via the base Visitor interface:\n";
+$visitor1 = new ConcreteVisitor1();
+clientCode($components, $visitor1);
+echo "\n";
+
+echo "It allows the same client code to work with different types of visitors:\n";
+$visitor2 = new ConcreteVisitor2();
+clientCode($components, $visitor2);
